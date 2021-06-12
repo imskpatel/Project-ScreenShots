1f222909258bf6b079110f3d01a2b6952834f352
Sunil <text2patelsunil@gmail.com>
2021-06-12 10:43:08 +0530

    new edit

--- shopify.js
+++ shopify.js
@@ -258,17 +258,8 @@ jQuery(document).ready(function() {
 			</div>
 		</div>
 		
-    </div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
+    </div> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>`);
 
